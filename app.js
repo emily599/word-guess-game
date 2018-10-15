73b000a2fa4aab4ef5c9143fae9f1efb927c6d06
Emily <emilydoan52@gmail.com>
2018-10-14 22:03:39 -0700

    started pseudo code

---
+++ app.js
@@ -0,0 +1,13 @@
+// 1. Make onkeyup function
+// 3. Make variable(var letterPress) that stores letter pressed
+// 4. Make a for loop to iterate through each letter of the word
+// 5. Use that var letterPress and compare that to letters in word
+//     -if letter exists in word, put correct letter in the word on html,
+//         -else letter does not exist in word, show "Wrong!" on the screen, put letter under "Wrong Letters guessed"
+// 6. after each letter pressed, subtract 1 from total 10 guesses
+// 7. Repeat for loop until word is complete
+// 8. randomly choose another word in Array
+
+
+
+
