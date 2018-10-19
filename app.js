75a5fd8075feb88badcdbf722f3a27053ad9bd01
Emily Doan <emilydoan52@gmail.com>
2018-10-18 18:12:04 -0700

    maxTries and incorrectlyGuessedLetters

--- app.js
+++ app.js
@@ -12,38 +12,27 @@ var selectableWords = [
     "mipha",
     "gerudo",
 ];
-
-const maxTries = 10;            // Maximum number of tries player has
+var incorrectlyGuessedLetters = []; //incorrect guesses
+var maxTries = 10;            // Maximum number of tries player has
 var guessedLetters = [];        // Stores the letters the user guessed
 var randomWord;                  // Index of the current word in the array
-var guessingWord = [];          // This will be the word we actually build to match the current word
-var remainingGuesses = 0;       // How many tries the player has left
+// var guessingWord = [];          // This will be the word we actually build to match the current word
+//var remainingGuesses = 0;       // How many tries the player has left
 var gameStarted = false;        // Flag to tell if the game has started
 var hasFinished = false;        // Flag for 'press any key to try again'     
 var wins = 0;                   // How many wins has the player racked up
 
 
-// 3. Make variable(var letterPress) that stores letter pressed
-// 4. Make a for loop to iterate through each letter of the word
-// 5. Use that var letterPress and compare that to letters in word
-//     -if letter exists in word, put correct letter in the word on html,
-//         -else letter does not exist in word, show "Wrong!" on the screen, put letter under "Wrong Letters guessed"
-// 6. after each letter pressed, subtract 1 from total 10 guesses
-
-// 1. Make onkeyup function
-// document.onkeydown = function (event) {
-// console.log(event.key);
-
-// }
-
-
-
-
-// When a key is pressed, the key is logged and shown on the page
+// When a key is pressed, the key is logged and shown on the page (if above 0)
 $("html").on("keydown", function (event) {
     console.log(event.key);
-    guessedLetters.push(event.key);
-    keyPress(event.key);
+    if (isEligible(maxTries)) {
+        keyPress(event.key);
+    } else if (!isEligible(maxTries)) {
+        alert("You lost");
+        $("lettersGuessed").empty();
+    }
+
 })
 
 
@@ -52,50 +41,47 @@ var randomWord = selectableWords[Math.floor(Math.random() * selectableWords.leng
 
 
 // shows underscores of letters for random word
-var underscoreArray = [];
+var underscoreArray = []; //correctly guessed letters
 for (i = 0; i < randomWord.length; i++) {
     underscoreArray.push("_ ");
 }
 $("#word").html(underscoreArray);
 
-
-function removeElement(guessedLetters, i) {
-    for (i = 0; i < array.length - 1; i++) {
-        if (array[i]) === remove
-    }
-}
-
 // comparing selected key to chosen word
 function keyPress(key) {
-    for (i = 0; i < randomWord.length; i++) {
-        if (randomWord.charAt(i) === key) {
-            console.log(key);
-            underscoreArray[i] = key;
-            $("#word").html(underscoreArray);
-
-        }
+    if (randomWord.includes(key)) {
+        console.log(key);
+        var i = randomWord.indexOf(key);
+        underscoreArray[i] = key;
+    } else if (!incorrectlyGuessedLetters.includes(key)) {
+        incorrectlyGuessedLetters.push(key);
+        maxTries--;
     }
-    $("#lettersGuessed").html(guessedLetters);
+
+    $("#word").html(underscoreArray);
+    $("#lettersGuessed").html(incorrectlyGuessedLetters);
+    $("#guessesLeft").html(maxTries);
+}
+
+// if number of guesses hits 0, use may no longer guess letters
+function isEligible(maxTries) {
+    if (maxTries > 0) {
+        return true;
+    }
+    else if (maxTries <= 0) {
+        return false;
+    }
 }
 
 
 
 
 
-// show how many lives left
-comments = function () {
-    remainingGuesses.innerHTML = "You have " + lives + " lives";
-    if (lives < 1) {
-        showLives.innerHTML = "Game Over";
-    }
-    for (var i = 0; i < geusses.length; i++) {
-        if (counter + space === geusses.length) {
-            showLives.innerHTML = "You Win!";
-        }
-    }
-}
-// 7. Repeat for loop until word is complete
-// 8. randomly choose another word in Array
+
+
+
+
+
 
 
 
